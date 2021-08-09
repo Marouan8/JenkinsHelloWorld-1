aa559b3bf2a97b924a285a5f7d24f67802d6542b
Marouan8 <54118521+Marouan8@users.noreply.github.com>
2021-08-09 11:53:19 +0200

    adding changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----***** Made some change with Poll SCM *****-----");
+		System.out.println("-----***** Made some changes*****-----");
 	}
 }
