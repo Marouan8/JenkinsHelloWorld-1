46bbac54a5ecc79081245542fff40929ce25f008
Marouan8 <54118521+Marouan8@users.noreply.github.com>
2021-08-09 14:19:49 +0200

    jenkins

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----***** Trying webhook with Jenkins, Last time *****-----");
+		System.out.println("-----***** Trying webhook with Jenkins, Last time for you my jenkins *****-----");
 		
 	}
 }
